93e70223a5556e18e589d3690e2cbe94308bcfe2
Amine Larbi <amine.larbi@icosnet.dz>
2024-01-03 16:27:05 +0100

    fixed more passwords and added isAdmin prop

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,15 +8,13 @@ import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UsersService {
-
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async create(createUserDto: CreateUserDto) {
-    
-    const { username, email} = createUserDto;
+    const { username, email } = createUserDto;
     const existByEmail = await this.checkifEmailExist(email);
     const existByUsername = await this.checkifUsernameExist(username);
-    
+
     if (existByEmail) {
       throw new NotFoundException('Email already exist');
     }
@@ -25,12 +23,10 @@ export class UsersService {
     }
     const user = new this.userModel(createUserDto);
     return user.save();
- 
   }
 
   findAll() {
-     
-    return this.userModel.find().exec();
+    return this.userModel.find().select('-password').exec();
   }
 
   async findOne(id: string): Promise<User | null> {
@@ -45,7 +41,7 @@ export class UsersService {
     }
   }
   async findByUserName(username: string): Promise<User | null> {
-    const user = await this.userModel.findOne({username:username}).exec();
+    const user = await this.userModel.findOne({ username: username }).exec();
     if (!user) {
       throw new NotFoundException('User not found');
     } else {
@@ -53,8 +49,11 @@ export class UsersService {
     }
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto): Promise<User | string> {
-    const { username = '', email = ''} = updateUserDto;
+  async update(
+    id: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<User | string> {
+    const { username = '', email = '' } = updateUserDto;
     if (username !== '') {
       const existByUsername = await this.checkifUsernameExist(username);
       if (existByUsername) {
@@ -67,15 +66,13 @@ export class UsersService {
         throw new NotFoundException('Email already exist');
       }
     }
-    
-    
+
     const exitingUser = await this.userModel
       .findByIdAndUpdate({ _id: id }, { $set: updateUserDto }, { new: true })
       .exec();
     if (!exitingUser) {
       throw new NotFoundException(`user ${id} not found`);
-     
-    }  
+    }
     return exitingUser;
   }
 
@@ -95,7 +92,10 @@ export class UsersService {
     }
   }
   async checkifEmailExist(email: string): Promise<boolean> {
-    const user = await this.userModel.findOne({ email: email }).exec();
+    const user = await this.userModel
+      .findOne({ email: email })
+      .select('-password')
+      .exec();
     if (!user) {
       return false;
     } else {
@@ -103,13 +103,14 @@ export class UsersService {
     }
   }
   async checkifUsernameExist(username: string): Promise<boolean> {
-    const user = await this.userModel.findOne({ username: username }).exec();
+    const user = await this.userModel
+      .findOne({ username: username })
+      .select('-password')
+      .exec();
     if (!user) {
       return false;
     } else {
       return true;
     }
-
   }
-
 }
