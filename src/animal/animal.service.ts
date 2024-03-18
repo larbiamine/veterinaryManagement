884c0b740b84cce578ed1bf61e47aa7a3914c289
Amine Larbi <amine.larbi@icosnet.dz>
2024-03-18 11:59:52 +0100

    cleaned animal service

--- src/animal/animal.service.ts
+++ src/animal/animal.service.ts
@@ -1,22 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAnimalDto } from './dto/create-animal.dto';
 import { UpdateAnimalDto } from './dto/update-animal.dto';
-import {  PrismaUncheckedAnimal } from './entities/animal.entity';
+import { PrismaUncheckedAnimal } from './entities/animal.entity';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { UtilitiesService } from 'src/utilities/utilities.service';
 
 @Injectable()
 export class AnimalService {
-  constructor(
-    private prisma: PrismaService,
-    private readonly utilitiesService: UtilitiesService,
-  ) {}
+  constructor(private prisma: PrismaService) {}
 
   async create(createAnimalDto: CreateAnimalDto) {
-    // if (!this.utilitiesService.areAllFieldsStrings(CreateOwnerDto, ['id', 'ownerId', 'vetId'])) {
-    //   throw new NotFoundException('all fields should be a string');
-    // }
-    // createAnimalDto.dateOfBirth = new Date(createAnimalDto.dateOfBirth);
     const animal = await this.prisma.animal.create({
       data: createAnimalDto,
     });
